2a1b5757a33733515f65689dc75de159f7bdda3f
Amir Hossein Jobeiri <ahjsg77@gmail.com>
2018-08-15 16:24:28 +0430

    users with uploading feature working

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -7,8 +7,6 @@
 
     {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'files' => true])!!}
 
-
-
         <div class="form-group">
             {!! Form::label('name', 'Name:') !!}
             {!! Form::text('name', null, ['class'=>'form-control']) !!}
@@ -30,8 +28,8 @@
         </div>
 
         <div class="form-group">
-            {!! Form::label('file', 'Choose Photo:') !!}
-            {!! Form::file('file', null, ['class'=>'form-control']) !!}
+            {!! Form::label('photo_id', 'Choose Photo:') !!}
+            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
         </div>
 
         <div class="form-group">
